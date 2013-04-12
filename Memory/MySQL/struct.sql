73f1459c1f81d4ebd74a679170fd86e400e45667
coffeine-009 <vitaliyacm@gmail.com>
2013-04-12 17:04:59 +0200

    DB Start

---
+++ Memory/MySQL/struct.sql
@@ -0,0 +1,37 @@
+/* Kafedra Differencial equation and Applied Mathematic :: Struct	***	*** */
+
+	/**	***	***	***	***	***	***	***	***	***	***	***	***	***	***	***	***	*
+	 *																	*
+	 *	@copyright 2013
+	 *		by
+	 *	@author Vitaliy Tsutsman
+	 *		and
+	 *	@author Andrii Plyasun
+	 *		and
+	 *	@author Andrii Khristonko
+	 *
+	 *	@date 2013-04-12 16:55:54 :: ....-..-.. ..:..:..
+	 *
+	 *	@address Poland/Krakow/Bydryka/5/414
+	 *
+	 *	@description ::
+	 *		Service for kafedra DEAM of PNU Ukraine
+	 *																	*
+	***	***	***	***	***	***	***	***	***	***	***	***	***	***	***	***	***	*/
+
+/*	Code	***	***	***	***	***	***	***	***	***	***	***	***	***	***	***	***	*/
+/* #- Create database -# */
+CREATE DATABASE `deam`
+	DEFAULT
+		CHARACTER SET utf8 
+		COLLATE utf8_general_ci;
+
+/* #- Set Datebase for using -# */
+USE `deam`;
+
+/* Create tables	***	***	***	***	***	***	***	***	***	***	***	***	***	***	*/
+/* -#  #- */
+CREATE TABLE ``(
+
+)
+ENGINE = InnoDB CHARACTER SET = utf8;
