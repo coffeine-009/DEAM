1f96822d383c63981d363c81730183ebbae6ec89
coffeine-009 <vitaliyacm@gmail.com>
2013-04-14 12:01:18 +0200

    Add controller professor

--- Memory/MySQL/struct.sql
+++ Memory/MySQL/struct.sql
@@ -30,8 +30,57 @@ CREATE DATABASE `deam`
 USE `deam`;
 
 /* Create tables	***	***	***	***	***	***	***	***	***	***	***	***	***	***	*/
-/* -#  #- */
-CREATE TABLE ``(
-	`id`		INTEGER AUTO_INCREMENT
+/* -# Role of users #- */
+CREATE TABLE `role`(
+	`id`			INTEGER AUTO_INCREMENT, /* Identificator 				*/
+	`title`			VARCHAR( 32 ) NOT NULL, /* Title of role[admin, teacher]*/
+	`description`	TEXT, 				/* Description about current role	*/
+
+	/* Keys */
+	PRIMARY KEY( `id` )
+)
+ENGINE = InnoDB CHARACTER SET = utf8;
+
+/* -# Data about users #- */
+CREATE TABLE `user`(
+	`id`			INTEGER AUTO_INCREMENT, /* Identificator */
+	`id_role`		INTEGER NOT NULL, 		/* Id of role */
+	/* Names */
+	`first_name`	VARCHAR( 20 ) NOT NULL, /* First name of user 	*/
+	`second_name`	VARCHAR( 20 ) NOT NULL, /* Second name of user 	*/
+	`father_name`	VARCHAR( 32 ) NOT NULL, /* Father name of user 	*/
+	/* Personal data */
+	`birthday`		DATE NOT NULL, 			/* Date of birthday 	*/
+	`post`			TEXT, 					/* Post of organization */
+	`creation`		TIMESTAMP DEFAULT CURRENT_TIMESTAMP, /* Time of create */
+
+	/* Keys */
+	PRIMARY KEY( `id` ), 
+
+	FOREIGN KEY( `id_role` ) REFERENCES `role`( `id` )
+		ON UPDATE CASCADE
+		ON DELETE RESTRICT
+)
+ENGINE = InnoDB CHARACTER SET = utf8;
+
+/* -# User contact: E-mail #- */
+CREATE TABLE `email`(
+	`id`			INTEGER AUTO_INCREMENT, /* Identificator */
+	`id_user`		INTEGER NOT NULL, 		/* Id of role */
+	/* Names */
+	`first_name`	VARCHAR( 20 ) NOT NULL, /* First name of user 	*/
+	`second_name`	VARCHAR( 20 ) NOT NULL, /* Second name of user 	*/
+	`father_name`	VARCHAR( 32 ) NOT NULL, /* Father name of user 	*/
+	/* Personal data */
+	`birthday`		DATE NOT NULL, 			/* Date of birthday 	*/
+	`post`			TEXT, 					/* Post of organization */
+	`creation`		TIMESTAMP DEFAULT CURRENT_TIMESTAMP, /* Time of create */
+
+	/* Keys */
+	PRIMARY KEY( `id` ), 
+
+	FOREIGN KEY( `id_role` ) REFERENCES `role`( `id` )
+		ON UPDATE CASCADE
+		ON DELETE RESTRICT
 )
 ENGINE = InnoDB CHARACTER SET = utf8;
