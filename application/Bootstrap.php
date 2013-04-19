3f949663cf4fb4af3a413a8c136e3702995c8d33
hadson19 <weerdon2008@gmail.com>
2013-04-19 14:24:24 +0200

    Change bootstrap
    add connect database

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,18 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
-
+	public function _initDataBase()
+	{
+		//- Get adapter -//
+		$db = $this -> getPluginResource( 'db' ) -> getDbAdapter();
+	
+		//- Set default adapter -//
+		//Zend_Db_Table :: setDefaultAdapter( $db );
+	
+		//- Registry adapter -//
+		Zend_Registry :: set( 'db', $db );
+			
+		Zend_Db_Table::setDefaultAdapter($db);
+	}
 }
 
